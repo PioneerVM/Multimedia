eaf6b9efe3a73b3e13ebcc34891e2ff60cc27d00
yindemin <821180585@qq.com>
2019-06-12 23:51:10 +0800

    添加ARouter
    Change-Id: I6258c824967bc5f2ca1c1613a1f19028c3c268d1

--- app/src/main/java/com/xinxin/multimedia/MainActivity.java
+++ app/src/main/java/com/xinxin/multimedia/MainActivity.java
@@ -2,6 +2,9 @@ package com.xinxin.multimedia;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.alibaba.android.arouter.launcher.ARouter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        findViewById(R.id.login).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                //无参数
+//                ARouter.getInstance().build("/home/AActivity") .navigation();
+//            }
+//        });
     }
 }
