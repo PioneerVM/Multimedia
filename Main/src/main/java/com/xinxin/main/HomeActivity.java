4794d353255caad7febdeba833a422a056dffc39
yindemin <821180585@qq.com>
2019-06-13 23:02:15 +0800

    启动页面初步搭建
    Change-Id: Icc53d4c551833b79e2171083596302b2e677e558

--- Main/src/main/java/com/xinxin/main/HomeActivity.java
+++ Main/src/main/java/com/xinxin/main/HomeActivity.java
@@ -3,11 +3,14 @@ package com.xinxin.main;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.alibaba.android.arouter.facade.annotation.Route;
+
+@Route(path = "/main/HomeActivity")
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.home_activity_main);
     }
 }
--- app/src/main/java/com/xinxin/multimedia/MainActivity.java
+++ app/src/main/java/com/xinxin/multimedia/MainActivity.java
@@ -1,23 +1,34 @@
 package com.xinxin.multimedia;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
 
 import com.alibaba.android.arouter.launcher.ARouter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        findViewById(R.id.login).setOnClickListener(new View.OnClickListener() {
+        setTheme(R.style.AppTheme);//恢复原有的样式
+        setContentView(R.layout.app_activity_main);
+        new Thread(new Runnable() {
             @Override
-            public void onClick(View view) {
-                //无参数
-                ARouter.getInstance().build("/home/LoginActivity") .navigation();
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        //无参数
+                        ARouter.getInstance().build("/main/HomeActivity").navigation();
+                        finish();
+                    }
+                });
             }
-        });
+        }).start();
     }
 }
